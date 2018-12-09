44a1c021d6b72b7bb8f0940141a6499d697daff8
Andie <zchangan@163.com>
2018-12-09 15:35:06 +0800

    :bug: Fix bug when geting null

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import {Pair} from 'kevast/dist/nodejs/Pair';
 import * as stream from 'stream';
 
 export = class KevastEncrypt implements IDuplexMiddleware {
-  public static randomKey(length: number): string {
+  public static randomKey(length: number = 32): string {
     return crypto.randomBytes(length / 2).toString('hex');
   }
   private algorithm: string;
@@ -25,11 +25,13 @@ export = class KevastEncrypt implements IDuplexMiddleware {
   }
   public async onGet(pair: Pair, next: () => Promise<void>) {
     await next();
-    const decipher = crypto.createDecipheriv(this.algorithm, this.key, this.iv, this.options);
-    let plainBuffer = decipher.update(pair[1], 'hex');
-    plainBuffer = Buffer.concat([plainBuffer, decipher.final()]);
-    const plain = plainBuffer.toString('utf8');
-    pair[1] = plain;
+    if (typeof pair[1] === 'string') {
+      const decipher = crypto.createDecipheriv(this.algorithm, this.key, this.iv, this.options);
+      let plainBuffer = decipher.update(pair[1], 'hex');
+      plainBuffer = Buffer.concat([plainBuffer, decipher.final()]);
+      const plain = plainBuffer.toString('utf8');
+      pair[1] = plain;
+    }
   }
   public async onSet(pair: Pair, next: () => Promise<void>) {
     const cipher = crypto.createCipheriv(this.algorithm, this.key, this.iv, this.options);
