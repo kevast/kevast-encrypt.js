b6ad303acd1d0944c06b6ca6b9ed4a1921b9ede2
Andie <zchangan@163.com>
2018-12-14 15:00:14 +0800

    :heavy_minus_sign: Remove @types/crypto-js for it's awful declaration

--- index.ts
+++ index.ts
@@ -1,29 +1,26 @@
-import * as crypto from 'crypto-js';
 import { IDuplexMiddleware } from 'kevast/dist/nodejs/Middleware';
 import { Pair } from 'kevast/dist/nodejs/Pair';
+import * as crypto from './lib/crypto';
 
 export = class KevastEncrypt implements IDuplexMiddleware {
   public static randomKey(length: number = 32): string {
-    return crypto.lib.WordArray.random(length / 2).toString();
+    return crypto.randomString(length);
   }
   private key: string;
-  private mode: crypto.Mode;
-  public constructor(key: string, mode: crypto.Mode = crypto.mode.CBC) {
+  public constructor(key: string) {
+    if (typeof key !== 'string') {
+      throw new TypeError('Key must be a string.');
+    }
     this.key = key;
-    this.mode = mode;
   }
   public async onGet(pair: Pair, next: () => Promise<void>) {
     await next();
     if (typeof pair[1] === 'string') {
-      const plain = crypto.AES.decrypt(pair[1], this.key, {
-        mode: this.mode,
-      }).toString(crypto.enc.Utf8);
+      const plain = crypto.decrypt(pair[1], this.key);
       pair[1] = plain;
     }
   }
   public async onSet(pair: Pair, next: () => Promise<void>) {
-    const encrypted = crypto.AES.encrypt(pair[1], this.key, {
-      mode: this.mode,
-    }).toString();
+    const encrypted = crypto.encrypt(pair[1], this.key);
     pair[1] = encrypted;
   }};
