7e2a16e1a4a072208826a5caf57fc89947bce916
Andie <zchangan@163.com>
2018-12-30 23:58:11 +0800

    :white_check_mark: Test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,32 +1,36 @@
 import assert = require('assert');
-import Kevast = require('kevast');
-import KevastMemory = require('kevast-memory');
-import KevastEncrypt = require('../index');
+import { Kevast } from 'kevast';
+import { KevastEncrypt } from '../index';
 
 describe('Test basic function', () => {
-  const kevast = new Kevast(new KevastMemory());
-  const key = KevastEncrypt.randomKey();
-  const veryLong: string = KevastEncrypt.randomKey(100000);
-  kevast.use(new KevastEncrypt(key));
-  it('Get null or default', async () => {
-    assert(await kevast.get('key1') === null);
-    assert(await kevast.get('key1', 'default') === 'default');
+  let kevast: Kevast;
+  let key: string;
+  let veryLong: string;
+  before(async () => {
+    kevast = await Kevast.create();
+    key = KevastEncrypt.randomKey();
+    veryLong = KevastEncrypt.randomKey(100000);
+    kevast.use(new KevastEncrypt(key));
+  });
+  it('Get null or default', () => {
+    assert(kevast.get('key1') === null);
+    assert(kevast.get('key1', 'default') === 'default');
   });
   it('Set normal', async () => {
     await kevast.set('key1', 'value1');
-    const onlyOne = [...(await kevast.values())][0];
+    const onlyOne = [...kevast.values()][0];
     assert(onlyOne !== 'value1');
   });
-  it('Get normal', async () => {
-    assert(await kevast.get('key1') === 'value1');
+  it('Get normal', () => {
+    assert(kevast.get('key1') === 'value1');
   });
   it('Set very long value', async () => {
     await kevast.delete('key1');
     await kevast.set('key2', veryLong);
-    const onlyOne = [...(await kevast.values())][0];
+    const onlyOne = [...kevast.values()][0];
     assert(onlyOne !== veryLong);
   });
   it('Get very long value', async () => {
-    assert(await kevast.get('key2') === veryLong);
+    assert(kevast.get('key2') === veryLong);
   });
 });
